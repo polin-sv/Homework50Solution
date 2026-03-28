a51d38b6edae2db25826deee504de970600bc945
polin-sv <polinasvetlay@icloud.com>
2026-03-28 21:48:50 +0300

    soft task03 recusion

---
+++ task03.01/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int find_max_digit(int number) {
+	if (number <= 9) {
+		return number;
+	}
+	int max = find_max_digit(number / 10);
+	return (number % 10 > max) ? number % 10 : max;
+}
+
+int main() {
+	int number;
+	cout << "input your number: ";
+	cin >> number;
+
+	cout << find_max_digit(number);
+
+	return 0;
+}
